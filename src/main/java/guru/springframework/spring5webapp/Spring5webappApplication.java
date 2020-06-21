415d4435c4ac7ad7a8e4c61b118c6c3c811b94e3
x215617 <X215617@prservicos.com.br>
2020-06-21 11:57:24 -0300

    Adicao de javadoc

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -3,6 +3,10 @@ package guru.springframework.spring5webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Bruno S. Alessi <bruno_alessi@yahoo.com.br>
+ * @since 2020-06-21
+ */
 @SpringBootApplication
 public class Spring5webappApplication {
 
