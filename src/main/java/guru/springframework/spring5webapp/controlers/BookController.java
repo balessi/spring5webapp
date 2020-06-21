b76cef3af208102f37aa6b7aa11e1fb9a9e499a5
x215617 <X215617@prservicos.com.br>
2020-06-21 11:30:14 -0300

    Criacao do BookController

---
+++ src/main/java/guru/springframework/spring5webapp/controlers/BookController.java
@@ -0,0 +1,29 @@
+package guru.springframework.spring5webapp.controlers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.spring5webapp.repositories.BookRepository;
+
+/**
+ * @author Bruno S. Alessi <bruno_alessi@yahoo.com.br>
+ * @since 2020-06-21
+ */
+@Controller
+public class BookController {
+
+	private final BookRepository bookRepo;
+
+	public BookController(BookRepository bookRepo1) {
+		super();
+		this.bookRepo = bookRepo1;
+	}
+
+	@RequestMapping("/books")
+	public String getBooks(Model model) {
+		model.addAttribute("books", this.bookRepo.findAll());
+		return "books";
+	}
+
+}
